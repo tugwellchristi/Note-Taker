1a31058c56c9d48d4c279db67e5ad304ee2fd030
tugwellchristi <tugwellchristi@gmail.com>
2023-12-02 13:28:14 -0800

    deleted develop folder, initial server.js content

---
+++ server.js
@@ -0,0 +1,25 @@
+//Import express and node package "path"
+const express = require('express');
+const path = require('path');
+
+// Identify the port of use and initialize the app express use
+const app = express();
+const PORT = 3001;
+
+// Middleware pointing to the public folder
+app.use(express.static('public'));
+
+// Create express routes for default '/', '*' and '/notes' endpoints
+app.get('/', (req, res) => res.send('Navigate to * or /notes'));
+     
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
