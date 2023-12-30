82e0b15bf0f422f382a7a145d8f166cf320898a0
tugwellchristi <tugwellchristi@gmail.com>
2023-12-30 15:59:41 -0800

    very minor changes to server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 //Import express and node package "path"
 const express = require('express');
 const path = require('path');
-const notes = require('./db/notes.json');
+const notes = require('./db/db.json');
 
 // Identify the port of use and initialize the app express use
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve images, css files, js files from the public directory
 // Allows us to reference files with their relative path
 app.use(express.static('public'));
-app.use(express.json());
+
 
 
 // Create express routes for default '/', '*' and '/notes' endpoints
